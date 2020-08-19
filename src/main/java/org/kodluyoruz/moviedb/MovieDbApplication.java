3686a2c2312fa7a51e31b0e46e48d3c0993dd2e6
Yağız Ünsal <yagizunsal@outlook.com.tr>
2020-08-19 13:08:28 +0300

    Initial commit

---
+++ src/main/java/org/kodluyoruz/moviedb/MovieDbApplication.java
@@ -0,0 +1,15 @@
+package org.kodluyoruz.moviedb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieDbApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(MovieDbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/kodluyoruz/moviedb/MovieDbApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kodluyoruz.moviedb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
